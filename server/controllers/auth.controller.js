958d5c845ccac7a816f7c1b6fe1f3895f6a7c18b
dev-koushal <koushalydv99@gmail.com>
2026-04-22 10:51:58 +0530

    minor

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -19,8 +19,8 @@ export const googleAuth = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure:"secure",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
     
