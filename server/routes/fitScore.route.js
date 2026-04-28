0286534b2fefd2304b71844632c5a4098eb32a6a
dev-koushal <koushalydv99@gmail.com>
2026-04-29 01:33:02 +0530

    remove: auth feature

--- server/routes/fitScore.route.js
+++ server/routes/fitScore.route.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { getFitScore } from "../controllers/interview.controller.js";
-import isAuth from "../middlewares/isAuth.middleware.js"
+
 const fitScorerouter = express.Router();
 
-fitScorerouter.post("/fit-score", isAuth, getFitScore);
+fitScorerouter.post("/fit-score",getFitScore);
 
 export default fitScorerouter;
